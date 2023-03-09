18dc8d34734d2dee8d9470669044f1ed52c45281
weiwentao <weiruowen@outlook.com>
2023-03-09 18:14:05 +0800

    live2d

--- src/page/home.js
+++ src/page/home.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Content from "../components";
+import ReactLive2d from 'react-live2d';
 
 const Home = () => {
 
     return (
         <>
            welcome <Content />
+            <ReactLive2d
+                ModelList = { ['Haru', 'heitaizi_2']}
+                width={300}
+                height={500}
+            />
         </>
     )
 
